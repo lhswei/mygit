01a91b0c3964426495e75d995c7351af2120ec7e
lhswei <609741@163.com>
2016-10-13 16:17:29 +0800

    mm

--- makegitalias.sh
+++ makegitalias.sh
@@ -1,5 +1,5 @@
-#git config --global user.name "lhswei"
-#git config --global user.email "609741@163.com"
-#git config --global color.ui "true" 
+git config --global user.name "lhswei"
+git config --global user.email "609741@163.com"
+git config --global color.ui "true" 
 cp .git-credentials ~/.git-credentials
 git config --global credential.helper store
